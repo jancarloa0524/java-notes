4d017e67ec270512ed03e5fb18316b77352907d7
jancarloa0524 <jancarloa0524@hstat.org>
2022-06-27 23:51:18 -0400

    completed hello-world section

---
+++ hello-world/HelloWorld.java
@@ -0,0 +1,31 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+    System.out.print("Goobye");
+
+    System.out.print(" World!");
+  }
+}
+
+// public class is the name of the file. A java program can have multiple of these. It represents a real-world idea. 
+// main runs a certain task
+// java should include white-space, curly braces, and semi-colons
+
+// System: Built in java class containing tools
+// out: output
+// println: print line
+// print: print text in current line
+
+/* 
+Multi
+Line
+Comments
+Still
+Exist!
+*/
+
+// Once done, use the terminal to compile your java code with:
+// javac ExampleName.java
+// And then run it with
+// java ExampleName
